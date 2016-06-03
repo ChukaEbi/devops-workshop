db9558d0a5ae6597c53a5a7b00980896d2201e27
Chuka Ebi <chuka.ebi@corp.fjordnet.com>
2016-06-03 13:11:28 +0100

    extension is always true

--- build/unicorn/app.js
+++ build/unicorn/app.js
@@ -4,7 +4,9 @@ var bodyParser = require('body-parser');
 
 var app = express();
 app.use(bodyParser.json({ type: 'application/json' }));
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get('/', function(req,res){
   res.json({ greeting: "Hello Workshoppers"});
